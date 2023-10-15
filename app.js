33a716643c835a2176072d35b52ec49302d0078f
Hyrekung <hyrekungch@gmail.com>
2023-10-15 23:21:22 +0700

    Finished

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 const screen = document.querySelector(".screen")
 var a, b, c, operator = "none"
-//document.addEventListener("click", NumberButton)
 
 function NumberButton(num) {
-    if (screen.innerHTML == 0) {
+     if (screen.innerHTML == 0) {
         screen.innerHTML = num
     } else {
-        // if (operator !== "none") {
-        // screen.innerHTML = num
         if (screen.innerHTML !== 0) {
             screen.innerHTML += num
         }
     }
-    // if (operator === "none") {
-    //   screen.innerHTML += num
-    //}
 
 }
 
-//}
 
 function Backspace() {
-    digit = (screen.innerHTML + "").length
+    digit = screen.innerHTML.toString().length
     screen.innerHTML = (screen.innerHTML + "").slice(0, digit - 1)
     if (screen.innerHTML == "") {
         screen.innerHTML = 0
@@ -64,33 +57,14 @@ function Equal() {
             operator = "none"
         }
     }
-    //alert("a = "+ a + ", b = "+ b + ", c = " +c+ ", operator = " + operator)
 }
 
 function Plus() {
     if (operator === "none") {
         a = screen.innerHTML
-        screen.innerHTML = 0
+     screen.innerHTML = 0
         operator = "+"
-        //alert("a = " + a + ", b = " + b + ", c = " + c + ", operator = " + operator)
-    }
-    else {
-        if (operator === "+") {
-            b = screen.innerHTML
-            c = parseInt(a) + parseInt(b)
-            screen.innerHTML = c
-            //alert("a = " + a + ", b = " + b + ", c = " + c + ", operator = " + operator)
-        }
-        if (operator === "−") {
 
-        }
-        if (operator === "×") {
-
-        }
-        if (operator === "÷") {
-
-        }
-        // alert("a = " + a + ", b = " + b + ", c = " + c + ", operator = " + operator)
     }
 }
 
