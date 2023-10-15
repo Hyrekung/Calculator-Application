a2e78a5b7e8978e1c7fffb3f8c74baaa9e522fe0
Hyrekung <hyrekungch@gmail.com>
2023-10-15 18:11:52 +0700

    fixing plus function

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const screen = document.querySelector(".screen")
 var a, b, c, operator = "none"
-var previousOperator = "none"
 //document.addEventListener("click", NumberButton)
 
 function NumberButton(num) {
     if (screen.innerHTML == 0) {
         screen.innerHTML = num
     } else {
-        if (previousOperator !== "none") {
-            screen.innerHTML = num
-            //if(screen.innerHTML !== 0){
-               // screen.innerHTML += num
-           // }
+       // if (operator !== "none") {
+           // screen.innerHTML = num
+            if(screen.innerHTML !== 0){
+                screen.innerHTML += num
+            }
         }
-        if (previousOperator === "none") {
-            screen.innerHTML += num
-        }
+       // if (operator === "none") {
+         //   screen.innerHTML += num
+        //}
 
     }
 
-}
+//}
 
 function Clear() {
     screen.innerHTML = 0
@@ -41,6 +40,7 @@ function Equal() {
 function Plus() {
     if (operator === "none") {
         a = screen.innerHTML
+        screen.innerHTML = 0
         operator = "+"
         //alert("a = " + a + ", b = " + b + ", c = " + c + ", operator = " + operator)
     }
@@ -48,10 +48,7 @@ function Plus() {
         if (operator === "+") {
             b = screen.innerHTML
             c = parseInt(a) + parseInt(b)
-            a = c
             screen.innerHTML = c
-            previousOperator = "+"
-            operator = "none"
             //alert("a = " + a + ", b = " + b + ", c = " + c + ", operator = " + operator)
         }
         if (operator === "−") {
